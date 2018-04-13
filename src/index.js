b803665000dcad23518315706f998f427bd17c90
bcrawford1994 <robert.c.crawford.19@dartmouth.edu>
2018-04-13 17:07:25 -0400

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up');
+//console.log('starting up');
+//const $ = require('jquery');
+
+//$('main').html('Here we go!');
+// change require to es6 import style
+import $ from 'jquery';
+let num = 0;
+setInterval (function()=>{
+  $('main').html('You have been on this page for ${num} seconds');
+  num = num + 1}, 1000);
+  //alert("You have been on this page for ${num} seconds");
+  //num = num +1 },1000);
+//$('main').html('You have been on this page for ${num} seconds' )
