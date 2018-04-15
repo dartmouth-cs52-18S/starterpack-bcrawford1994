d506a2c8561799f848b792945af90077eff38b6e
bcrawford1994 <robert.c.crawford.19@dartmouth.edu>
2018-04-15 19:43:17 -0400

    update starter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-// console.log('starting up');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// const $ = require('jquery');
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
 
-// import './style.scss';
-// change require to es6 import style
-import $ from 'jquery';
-
-$('main').html('Here we go!');
-// let num = 0;
-// setInterval (function() =>{
-// $('main').html('You have been on this page for ${num} seconds');
-// num = num + 1}, 1000);
-// alert("You have been on this page for ${num} seconds");
-// num = num +1 },1000);
-// $('main').html('You have been on this page for ${num} seconds' )
+ReactDOM.render(<App />, document.getElementById('main'));
