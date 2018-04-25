8657a726b20685d7d8ac102de786f251921b8bf2
bcrawford1994 <robert.c.crawford.19@dartmouth.edu>
2018-04-25 17:09:10 -0400

    sa5 update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-// console.log('starting up');
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import React from 'react';
+import { Switch } from 'react-router';
+import ReactDOM from 'react-dom';
+import '../src/style.scss';
 
-// const $ = require('jquery');
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
 
-// import './style.scss';
-// change require to es6 import style
-import $ from 'jquery';
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-$('main').html('Here we go!');
-// let num = 0;
-// setInterval (function() =>{
-// $('main').html('You have been on this page for ${num} seconds');
-// num = num + 1}, 1000);
-// alert("You have been on this page for ${num} seconds");
-// num = num +1 },1000);
-// $('main').html('You have been on this page for ${num} seconds' )
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
